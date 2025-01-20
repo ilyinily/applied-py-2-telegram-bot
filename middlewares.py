4c33d7429481301bf409644b86752dc87b04f5c5
Ilia Ilin <ilyinily@gmail.com>
2025-01-21 00:51:30 +0300

    fixed middlewares.py

--- middlewares.py
+++ middlewares.py
@@ -4,6 +4,6 @@ from aiogram.types import Message
 class LoggingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict):
         print(f"Message received: {event.text}")
-        return await handler(event.data)
+        return await handler(event, data)
 
 
